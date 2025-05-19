04f14f067d9ad4da6566013a466b35bbbec8c5ce
JustusSager <justus.sager@gmail.com>
2025-05-19 09:34:22 +0200

    einiges an änderungen

--- configuration.nix
+++ configuration.nix
@@ -39,10 +39,40 @@
     LC_TELEPHONE = "de_DE.UTF-8";
     LCTIME = "de_DE.UTF-8";
   };
+  
+  # setup the window manager
+  services.xserver = {
+    enable = false;
+    
+    #desktopManager = {
+    #  xterm.enable = false;
+    #};
+    #
+    #displayManager = {
+    #  defaultSession = "none+i3";
+    #};
+    #
+    #windowManager.i3 = {
+    #  enable = true;
+    #  extraPackages = with pkgs; [
+    #    dmenu     # application launcher
+    #    i3status  # default i3 status bar
+    #    i3lock    # default i3 lock screen
+    #    #i3blocks  # 
+    #  ];
+    #};
+  };
 
-  # enable/disable the X11 windowing system
-  services.xserver.enable = false;
-
+  # hyprland setup
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+  
   # configure console keymap
   console.keyMap = "de";
 
@@ -57,7 +87,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
+    # If you want to uuse JACK applications, uncomment this
     # jack.enable = true;
 
     # use the example sission manager (no others are packaged yet so this is enabled by default)
@@ -85,51 +115,119 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Install Software with Home Manager ------------------------------
   # Install firefox
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    languagePacks = [ "de" "en-US" ];
+    policies = {
+      # https://mozilla.github.io/policy-templates/
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisableFirefoxAccounts = true;
+      # Lesezeichenleiste anzeigen? ["always" "newtab" "never"]     
+      DisplayBookmarksToolbar = "newtab";
+      # Menüleiste anzeigen? ["always" "default-on" "default-off" "never"]
+      DisplayMenuBar = "default-off";
+      # Suchleiste und Adressleiste trennen? ["unified" "seperate"]
+      SearchBar = "unified";
+      # Anbieten Passwörter zu speichern?
+      OfferToSaveLogins = false;
+      # Passwortmanager deaktivieren?
+      PasswordManagerEnabled = false;
+    };
+  };
+  
+  # VirtualBox Setup
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = [ "findus" ];
+
+  # Docker Setup
+  virtualisation.docker.enable = true;
+  # virtualisation.podman.enable = true;
+
 
   # List packages installed in system profile. To search run 'nix search <package name>'
   environment.systemPackages = with pkgs; [
-    # Basic Wayland must have stuff
-    foot # simple terminal
-    wofi # search bar / application launcher
-    dolphin # file manager
-    vim # cli text editor
-    nano # cli text editor
-    wget # cli file downloader
+    # ------------- Basic Wayland must have stuff ---------------
+    wofi  # search bar / application launcher
     waybar # task bar
     wl-clipboard # clipboard functionalities
+    wget # cli file downloader
+    mako # notification daemon
+    
+    # Terminal
+    foot   # simple terminal
+    #kitty # terminal evtl benötigt für neovim
+    
+    # file manager
+    dolphin                 # file manager
+    #kdePackages.qtsvg     # dolphin needs some symbols
+    #libsForQt5.kio-extras # mooore dolpin symbols
+    file-roller             # Zip Archive
+    
+    # Text Editors
+    vim      # cli text editor
+    nano     # cli text editor
+    sublime  # gui text editor
+
+    # Webbrowser
     firefox # web browser
+
+    # ----------------- Softwareentwicklung ---------------------
+    #neovim # better terminal text editor
     git # cli git
-    # personal stuff
-    neovim # better terminal text editor
-    nautilus # better file browser
-    # kitty # terminal evtl benötigt für neovim
-    # Die Jetbrains IDE Palette und was dafür nötig ist
-    jetbrains.idea-ultimate         # IntelliJ 
-    jetbrains.clion                 # CLion
+    gitg # git gui
+    docker # Docker Container
+    docker-compose # Docker compose Erweiterung für docker
+    #podman # managing pods, containers and container images
+    #podman-desktop # GUI für Podman Container
+    
+    # Java, Kotlin
+    jetbrains.idea-ultimate  # IntelliJ 
+    
+    # C, C++
+    jetbrains.clion  # CLion
+    cmake            # - CMake
+    ninja            # - build tool
+    gcc              # - C / C++ Compiler
+    sfml             # - Brauch ich evtl nicht
+    #xorg.libX11      # - Nötig für die CMake SFML Entwicklung
+    #xorg.libXi
+    #xorg.libXext
+    #xorg.xorgproto
+    #xorg.libXcursor
+    #xorg.libXinerama
+    
+    # Python
     jetbrains.pycharm-professional  # PyCharm
-    jetbrains.webstorm              # WebStorm
-    gcc                             # C/C++ compiler
-    python313                       # python3 interpreter
-    conda                           # python package manager
-    # Office
+    python313                       # - python3 interpreter
+    conda                           # - python package manager
+    
+    # Web
+    jetbrains.webstorm  # WebStorm
+    
+    # ---------------------- Office -----------------------------
     obsidian # notes
     thunderbird # email client
-    sublime # gui text editor
-    
+    discord-ptb # Discord halt
+    #qgis-ltr # QGIS läuft leider nicht mit Hyprland wird ausgelagert in eine VM
   ];
 
-  # hyprland setup
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
+  # Schriftarten
+  fonts.enableDefaultPackages = true;
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    fira-code
+    fira-code-symbols
+    dina-font
+    proggyfonts
+    open-sans
+  ];
+
 
   # Some programs need SUID wrappers, can be configured further or started in user sessions.
   # programs.mtr.enable = true;
